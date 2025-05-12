3b3892e26e55a351daa7e14652d5f37e2ddd154e
mossy-isme <mossycluney@gmail.com>
2025-05-12 13:56:53 +1000

    modified:   main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, request, send_file
+from flask_cors import CORS
 import subprocess
 import os
 import tempfile
 import zipfile
 
 app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return "🎉 M3U8 Downloader is running! POST to /download to use it."
 
 @app.route('/download', methods=['POST'])
 def download_m3u8():
