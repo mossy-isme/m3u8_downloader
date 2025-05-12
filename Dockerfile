dba3f89c6a445915090d4480c00ecc17b7f20659
mossy-isme <mossycluney@gmail.com>
2025-05-12 13:49:56 +1000

    modified:   Dockerfile 	modified:   main.py

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ WORKDIR /app
 COPY . .
 RUN pip install -r requirements.txt
 
-EXPOSE 8000
+EXPOSE 8080
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -29,4 +29,5 @@ def download_m3u8():
     return send_file(zip_path, as_attachment=True, download_name='videos.zip')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8080))  # use 8080 from Railway
+    app.run(host='0.0.0.0', port=port)
